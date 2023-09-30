40971a56d2e09476ed6552ab314e1b20ba026099
enos <enosgomesdev@gmail.com>
2023-09-30 01:23:59 -0300

    tipos basicos python

--- enos.py
+++ enos.py
@@ -1,13 +1,16 @@
-#Comentarios em Python sao com #
-#Crie um programa que pegue o valor de uma conta
-#de restaurante e divida pelo numero de pessoas
-#Por fim some o valor da gorjeta em cima do 
-#valor final
-
-contaRestaurante = int(input("Qual o valor da conta: "))
-numeroPessoas = int(input("Dividir para quantas pessoas: "))
-porcentagemGorjeta = int(input("Qual a porcentagem da gorgeta: "))
-
-valorCadaPessoa = (((contaRestaurante * porcentagemGorjeta ) / 100 ) + contaRestaurante ) / numeroPessoas
-
-print(valorCadaPessoa)
+nome = "enos"
+print(type(nome))
+nome = 1
+print(type(nome))
+nome = 1.1
+print(type(nome))
+nome = 4+10j # z = x+yj, onde j é imaginário
+print(type(nome))
+nome = True
+print(type(nome))
+nome = ["enos"]
+print(type(nome))
+nome = {"enos":1}
+print(type(nome))
+nome = ('enos','gomes')
+print(type(nome))
